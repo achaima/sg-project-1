ccbe3bc4a80a8f9e3585905468ed88f4b0148a0f
Asma Chaima <achaima@spartaglobal.co>
2017-03-05 23:43:10 +0000

    character successfully jumping and one object hovering

--- main.js
+++ main.js
@@ -1,19 +1,74 @@
 var canvas = document.getElementById('canvas1');
 var context = canvas.getContext('2d');
-var burger = new createImage('images/burger.png', 40, 40, 180, 100);
+var burger = new createImage('images/burger.png', 40, 40, Math.floor(Math.random() * (1320 - 200)) + 200, Math.random()* 420, -1, 0);
+var character = new createImage('images/character.png', 60, 60, 50, 380, 0, 0, 20, 0.4);
+
+var numBurger;
+var burgerArray;
 
 
+//EVENTS ----------------------------------------------------------------------
 
-// GAME LOOP ----------------------------------------------------------------
+$(function(){
+
+
+  // Directional Control SPACE KEY
+  $(document).keydown(function(key) {
+    if(key.keyCode === 32) {
+      character.vy = -5;
+    }
+  });
+});
+
+
+// MAIN LOOP ----------------------------------------------------------------
+
 mainLoop();
 function mainLoop() {
 
+  //burger movement
+  numBurger === 20;
+  burgerArray = new Array();
+
+  // for(var i = 0; i < numBurger; i++) {
+  //   var image = new Image;
+  //   var imagesrc = 'images/burger.png';
+  //   var width = 40;
+  //   var height = 40;
+  //   var x =  Math.floor(Math.random() * (1320 - 200)) + 200;
+  //   var y = Math.random()* 420;
+  //   var vx = -1;
+  //   var vy = 0;
+  //
+  //   burgerArray.push(new burger(image,imagesrc, x, y, width, height, vx, vy));
+  // }
+
+
+ //Burger movement
+  burger.x += burger.vx;
+  character.y += character.vy;
+
+  //WEIGHT and GRAVITY CHARACTER
+  if(character.vy < character.gravity) character.vy += character.weight;
+
+  //CANVAS BOUNDARIES
+  //TOP BOUNDARY
+  if (character.y <= 0) {
+    character.vy = 1;
+  }
+//BOTTOM BOUNDARY
+  if (character.y >= canvas.height - (character.height+40)) {
+    character.vy = 0;
+  }
+
+//CANVAS
   context.clearRect(0, 0, canvas.width, canvas.height);
   canvas.width = 1320;
-  canvas.height = 350;
-  canvas.style = 'border: 3px solid black; text-align: center; margin-left:50px; background-color: #87CEFA' ;
+  canvas.height = 520;
+  canvas.style = 'border: 3px solid black; text-align: center; margin-left:50px; background-image: url("images/back.png")' ;
 
- // Burger
+ // NON-STATIONARY DRAWINGS
+  context.drawImage(character.image, character.x, character.y, character.width, character.height);
   context.drawImage(burger.image, burger.x, burger.y, burger.width, burger.height);
 
 // Loop Time
@@ -21,11 +76,15 @@ function mainLoop() {
 }
 
 // OBJECTS ------------------------------------------------------------------
-function createImage(img, w, h, x, y) {
+function createImage(img, w, h, x, y, vx, vy, g, wt) {
   this.image = new Image();
   this.image.src = img;
   this.width = w;
   this.height = h;
   this.x = x;
   this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.gravity = g;
+  this.weight = wt;
 }
